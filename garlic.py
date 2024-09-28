ae9b8f41b752f6c7aeb95470071d7bcf86e2f912
Tiensa Tran <tiensa_tran@sfu.ca>
2024-09-28 13:54:31 -0700

    Added more garlics

--- garlic.py
+++ garlic.py
@@ -1,5 +1,6 @@
 import pygame
 
+from random import randrange
 from random import randint
 
 pygame.init()
@@ -12,30 +13,42 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 class Garlic(pygame.sprite.Sprite):
     def __init__(self, x, y, scale):
-        pygame.sprite.Sprite.init(self)
+        pygame.sprite.Sprite.__init__(self)
 
-        garlicImg = pygame.image.load('asset/garlic.png')
-        self.garlic = pygame.transform.scale(self.garlic, (int(garlicImg.get_width()*scale), garlicImg.get_height()*scale)) # scale to screen
+        garlicImg = pygame.image.load('assets/garlic.png')
+        self.garlic = pygame.transform.scale(garlicImg, (int(garlicImg.get_width()*scale), garlicImg.get_height()*scale)) # scale to screen
         # create a rectangle object
         self.rect = self.garlic.get_rect()
         self.rect.center = (x,y)
 
-    def draw(self):
+    def draw(self, surface):
         screen.blit(self.garlic, self.rect)
 
-garlic = Garlic(400, 250, 0.5)
+    def update(self):
+        speed = 5
+        self.rect.x -= speed
 
+garlic_sprites = []
+numGarlics = 10
+#garlic = Garlic( 400, 250, 0.5)
 
+for i in range(numGarlics):
+     x = randint(SCREEN_WIDTH, SCREEN_WIDTH+10000)
+     garlic = Garlic( x, 250, 0.5)
+     garlic_sprites.append(garlic)
 
 run = True
 while run:
     screen.fill("indigo")
 
-    garlic.draw(screen)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    for garlic in garlic_sprites: 
+
+        garlic.update()
+        garlic.draw(screen)
 
     pygame.display.flip()
 
