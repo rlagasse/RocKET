7ffdf2904a664d98cc51c2fac9dffabab83e0098
rlagasse <rlagasse@sfu.ca>
2024-09-28 11:07:36 -0700

    added gravity and jump

--- get_home.py
+++ get_home.py
@@ -9,15 +9,25 @@ pygame.init()
 
 screen_width = 1024
 screen_height = 512
+gravity = 0.5
+FLOOR = screen_height - 50
 screen = pygame.display.set_mode((screen_width, screen_height))
 background = pygame.image.load('background_filler.png')
 pygame.display.set_caption('Get Home')
 
+clock = pygame.time.Clock()
+FPS = 60
+
+
+
 class Vampire(pygame.sprite.Sprite):
     def __init__(self, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.health = 100
         self.speed = speed
-
+        self.jump = False # jumping logic
+        self.jump_velocity = 0
         vamp_img = pygame.image.load('vampire_filler.png')
         self.vampire = pygame.transform.scale(vamp_img, (int(vamp_img.get_width()*scale), vamp_img.get_height()*scale)) # scale to screen
         # create a rectangle object
@@ -38,12 +48,31 @@ class Vampire(pygame.sprite.Sprite):
         if right:
             dx = self.speed
 
+        if self.jump: # jump detected
+            self.jump_velocity = -2
+            self.jump = False
+
+        self.jump_velocity += gravity # come back down with gravity
+        if self.jump_velocity > 10:
+            self.jump_velocity 
+        dy += self.jump_velocity
+        
+        # added floor
+        if self.rect.bottom + dy > FLOOR:
+            dy = FLOOR - self.rect.bottom
+        
         self.rect.x += dx
         self.rect.y += dy
 
     def draw(self):
         screen.blit(self.vampire, self.rect)    # draw the vampire on the screen
 
+    # def gravity(self):
+    #     self.movey += 3.2
+        
+    #     if self.rect.y > screen_height and self.movey >= 0:
+    #         self.movey= 0
+    #         self.rect.y = screen_height-ty-ty
 
 # generate vampire in center of screen
 user = Vampire(400, 320, 0.5, 1) # x, y, scale, speed
@@ -57,6 +86,8 @@ while game:
     # background image
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
+    pygame.draw.line(screen, (255, 0, 0), (0, FLOOR), (screen_width, FLOOR))
+
     for event in pygame.event.get():
 
         # end game
@@ -69,39 +100,13 @@ while game:
     down = keys[pygame.K_s]
     left = keys[pygame.K_a]
     right = keys[pygame.K_d]
+    jump = keys[pygame.K_SPACE]
+    if jump and user.alive: # user jumped
+        user.jump = True
+    #user.gravity()
 
     user.move(up, down, left, right)
     user.draw() # create user to screen
-
-
-
-
-        # # keyboard button press and add to movement
-        # if event.type == KEYDOWN:
-        #     if event.key == (pygame.K_UP or pygame.K_w):
-        #         moving_up = True
-        #     elif event.key == (pygame.K_DOWN or pygame.K_s):
-        #         moving_down = True
-        #     elif event.key == (pygame.K_LEFT or pygame.K_a):
-        #         moving_left = True
-        #     elif event.key == (pygame.K_RIGHT or pygame.K_d):
-        #         moving_right = True
-        #     elif event.key == pygame.K_ESCAPE:
-        #         game = False
-        
-        # # keyboard button release
-        # if event.type == KEYUP:
-        #     if event.key == (pygame.K_UP or pygame.K_w):
-        #         moving_up = True
-        #     elif event.key == (pygame.K_DOWN or pygame.K_s):
-        #         moving_down = True
-        #     elif event.key == (pygame.K_LEFT or pygame.K_a):
-        #         moving_left = True
-        #     elif event.key == (pygame.K_RIGHT or pygame.K_d):
-        #         moving_right = True
-        
-
-
 
     pygame.display.update() # display updates
 
