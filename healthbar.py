c0b0800192897d19b019dc32c9769bfb6dec4f24
Tiensa Tran <tiensa_tran@sfu.ca>
2024-09-28 15:26:27 -0700

    changed healthbar colours

--- healthbar.py
+++ healthbar.py
@@ -25,8 +25,8 @@ class HealthBar:
 
     def draw(self, surface):
         ratio = self.hp / self.max_hp
-        pygame.draw.rect(surface, "red", (self.x, self.y, self.w, self.h))
-        pygame.draw.rect(surface, "green", (self.x, self.y, self.w * ratio, self.h))
+        pygame.draw.rect(surface, "white", (self.x, self.y, self.w, self.h))
+        pygame.draw.rect(surface, "red", (self.x, self.y, self.w * ratio, self.h))
 
 class Player:
     def get_damage(self, amount):
